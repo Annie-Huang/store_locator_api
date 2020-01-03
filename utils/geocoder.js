decb2309dfea1d9f77ae69b6a55f0528d9c1c5ff
Annie Huang <annie.hn@gmail.com>
2020-01-03 17:07:03 +1100

    Update geocoder.js with our variables info.

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -3,17 +3,15 @@
 const NodeGeocoder = require('node-geocoder');
 
 const options = {
-    provider: 'google',
+    // provider: 'google',
+    provider: process.env.GEOCODER_PROVIDER,
 
     // Optional depending on the providers
     httpAdapter: 'https', // Default
-    apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
+    apiKey: process.env.GEOCODER_API_KEY, // for Mapquest, OpenCage, Google Premier
     formatter: null         // 'gpx', 'string', ...
 };
 
 const geocoder = NodeGeocoder(options);
 
-// Using callback
-geocoder.geocode('29 champs elysée paris', function(err, res) {
-    console.log(res);
-});
+module.exports = geocoder;
