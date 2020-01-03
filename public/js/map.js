0ab089d105c4170ca1672eef2646961c995f0af7
Annie Huang <annie.hn@gmail.com>
2020-01-03 20:48:40 +1100

    Create loadMap function and copy the coordinate from mapboxgl.Map to here.

--- public/js/map.js
+++ public/js/map.js
@@ -7,3 +7,32 @@ const map = new mapboxgl.Map({
     zoom: 9,
     center: [-71.157895, 42.707741]
 });
+
+// Copy from https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+function loadMap() {
+    map.on('load', function() {
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': [
+                        {
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [-71.157895, 42.707741]
+                            }
+                        }
+                    ]
+                }
+            },
+            'layout': {
+                'icon-image': 'cat',
+                'icon-size': 0.25
+            }
+        });
+    });
+}
