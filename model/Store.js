38f354f67935d399ccaee0196a1fd203e15fcfc3
Annie Huang <annie.hn@gmail.com>
2020-01-03 15:28:39 +1100

    Update the StoreSchema in Store.js for extra info

--- model/Store.js
+++ model/Store.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // the location info is copy from https://mongoosejs.com/docs/geojson.html
+// 2dsphere supports queries that calculate geometries on an earth-like sphere
 const StoreSchema = new mongoose.Schema({
     storeId: {
         type: String,
@@ -9,15 +10,25 @@ const StoreSchema = new mongoose.Schema({
         trim: true,
         maxlength: [10, 'Store ID must be less than 10 chars']
     },
+    address: {
+        type: String,
+        required: [true, `Please add an address`]
+    },
     location: {
         type: {
             type: String, // Don't do `{ location: { type: String } }`
             enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
         },
         coordinates: {
             type: [Number],
-            required: true
-        }
+            index: '2dsphere'
+        },
+        formattedAddress: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
+
+module.exports = mongoose.model('Store', StoreSchema);
