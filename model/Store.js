31a4d666302eb8253653ab1648ec6f4e291a5cd2
Annie Huang <annie.hn@gmail.com>
2020-01-03 17:36:57 +1100

    Add geocoder middleware to get the StoreSchema (at the precommit stage) to translate address into location (and remove address) before storing it into Atlas.

--- model/Store.js
+++ model/Store.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
+
 
 // the location info is copy from https://mongoosejs.com/docs/geojson.html
 // 2dsphere supports queries that calculate geometries on an earth-like sphere
@@ -29,6 +31,45 @@ const StoreSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// Geocode & create location
+// See MongoDBAtlasII.docx screenshot for the final output.
+StoreSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+
+    // console.log(loc);
+    // You will see this in terminal after you delete the record in Atlas ans re-ingest it through postman
+    /*
+        MongoDB connected demo-cluster-shard-00-00-4fb1w.mongodb.net
+        [
+          {
+            formattedAddress: '10 Main St, Haverhill, MA 01830-6201, US',
+            latitude: 42.774757,
+            longitude: -71.076481,
+            country: null,
+            city: 'Haverhill',
+            stateCode: 'MA',
+            zipcode: '01830-6201',
+            streetName: '10 Main St',
+            streetNumber: null,
+            countryCode: 'US',
+            provider: 'mapquest'
+          }
+        ]
+     */
+
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress
+    };
+
+    // Do not save address
+    this.address = undefined;
+
+    // Middleware need to call next.
+    next();
 });
 
 module.exports = mongoose.model('Store', StoreSchema);
