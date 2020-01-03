237110364de3b3647243ebbe9365cdad669638c1
Annie Huang <annie.hn@gmail.com>
2020-01-03 17:50:01 +1100

    Set static folder serve by the express in server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.json());
 // Enable cors
 app.use(cors());
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // app.get('/api/v1/stores', (req, res) => {
 //     res.send('Hello');
 // });
